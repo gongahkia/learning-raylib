16ebd2e99fcab6bbf98119024cd7a7c93edfef0a
gong <angryapplegravy@gmail.com>
2023-03-01 13:36:46 +0800

    new

---
+++ README.md
@@ -0,0 +1,8 @@
+# Experimenting with Raylib
+
+Ever since I stumbled onto [**Raylib**](https://www.raylib.com/examples.html) on [Github](https://github.com/raysan5/raylib), I've been interested in learning more about how it works. 
+This repo will chart my journey.  
+
+~ Gong :) 
+
+![](https://www.meme-arsenal.com/memes/f691630cbf08ba9121b97105105a74ce.jpg)
---
+++ first_proj/main.c
@@ -0,0 +1,32 @@
+#include "raylib.h"
+
+
+int main(){
+
+    float character_pos[] = {0,0};
+
+    InitWindow(800, 500, "testing");
+    SetTargetFPS(60);
+   
+
+    while (!WindowShouldClose()){
+    
+    if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP)) character_pos[1] -= 10.0f;
+    if (IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN)) character_pos[1] += 10.0f;
+    if (IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT)) character_pos[0] -= 10.0f;
+    if (IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT)) character_pos[0] += 10.0f;
+
+    BeginDrawing();
+        ClearBackground(RAYWHITE);
+        
+        DrawCircle(character_pos[0], character_pos[1], 20.0, MAGENTA);
+
+        DrawText("Testing", 180, 200, 20, LIGHTGRAY);
+
+    EndDrawing();
+    }
+    
+    CloseWindow();
+
+    return 0;
+}
